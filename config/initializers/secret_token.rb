aadb2a8d9e984ab047c587fa8cbffc326bda17ac
Larry Scott <iamjustfresh@gmail.com>
2013-05-16 00:34:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telemagic::Application.config.secret_token = 'f22495c95d7a04033daf8ae7ce37ba6da1a0330991de7cfa45c0a320680bc084a901b9fd4e90851810354394b23b59fb57cae735e9aa6a320fba8ba73df454cc'
